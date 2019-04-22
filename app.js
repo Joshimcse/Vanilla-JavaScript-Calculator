2b889cb3d6c7ae97c9ce980537768bdbc811ee9b
joshimcse <joshimcse@gmail.com>
2019-04-22 23:38:24 +0600

    store dom element as a variable

---
+++ app.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-del]');
+const allClearButton = document.querySelector('data-ac');
+
+const prevOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand');
